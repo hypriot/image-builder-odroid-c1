ba99fa2e7a889875d6ade829cb438b2c3fe1756c
Dieter Reuter <dieter.reuter@me.com>
2016-01-10 19:15:44 +0100

    install odroid c1 kernel

--- build.sh
+++ build.sh
@@ -59,9 +59,20 @@ export DEST=$(readlink -m /etc/resolv.conf)
 mkdir -p $(dirname $DEST)
 echo "nameserver 8.8.8.8" > $DEST
 
+# set up odroid repository
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB19BAC9
+echo "deb http://deb.odroid.in/c1/ trusty main" > /etc/apt/sources.list.d/odroid.list
+echo "deb http://deb.odroid.in/ trusty main" >> /etc/apt/sources.list.d/odroid.list
+
 # install parted (for online disk resizing)
 apt-get update
 apt-get install -y parted
+
+# install odroid kernel
+export DEBIAN_FRONTEND=noninteractive
+mkdir -p /media/boot
+apt-get install -y u-boot-tools
+apt-get install -y linux-image-c1
 
 # set device label
 echo 'HYPRIOT_DEVICE="ODROID C1"' >> /etc/os-release
