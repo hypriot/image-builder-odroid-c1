07e18e54c874242739b8cbb413a2a45bf143e6b0
Dieter Reuter <dieter.reuter@me.com>
2016-01-11 20:12:39 +0100

    add hypriot customizing

--- devicefiles/build.sh
+++ devicefiles/build.sh
@@ -19,48 +19,52 @@ BUILD_PATH="/build"
 
 # where to store our base file system
 ROOTFS_TAR="rootfs-armhf.tar.gz"
-ROOTFS_TAR_PATH="${BUILD_RESULT_PATH}/${ROOTFS_TAR}"
+ROOTFS_TAR_PATH="$BUILD_RESULT_PATH/$ROOTFS_TAR"
 
 # size of root and boot partion
 ROOT_PARTITION_SIZE="800M"
 
 # create build directory for assembling our image filesystem
-rm -rf "${BUILD_PATH}"
-mkdir -p "${BUILD_PATH}"
+rm -rf $BUILD_PATH
+mkdir -p $BUILD_PATH
 
 # download our base root file system
-if [ ! -f "${ROOTFS_TAR_PATH}" ]; then
-  wget -q -O ${ROOTFS_TAR_PATH} https://github.com/hypriot/os-rootfs/releases/download/v0.4/${ROOTFS_TAR}
+if [ ! -f $ROOTFS_TAR_PATH ]; then
+  wget -q -O $ROOTFS_TAR_PATH https://github.com/hypriot/os-rootfs/releases/download/v0.4/$ROOTFS_TAR
 fi
 
 # extract root file system
-tar -xzf "${ROOTFS_TAR_PATH}" -C "${BUILD_PATH}"
+tar -xzf $ROOTFS_TAR_PATH -C $BUILD_PATH
 
 # register qemu-arm with binfmt
-update-binfmts --enable qemu-"${QEMU_ARCH}"
+update-binfmts --enable qemu-$QEMU_ARCH
 
 # set up mount points for pseudo filesystems
-mkdir -p ${BUILD_PATH}/{proc,sys,dev/pts}
+mkdir -p $BUILD_PATH/{proc,sys,dev/pts}
 
-mount -o bind /dev "${BUILD_PATH}/dev"
-mount -o bind /dev/pts "${BUILD_PATH}/dev/pts"
-mount -t proc none "${BUILD_PATH}/proc"
-mount -t sysfs none "${BUILD_PATH}/sys"
+mount -o bind /dev $BUILD_PATH/dev
+mount -o bind /dev/pts $BUILD_PATH/dev/pts
+mount -t proc none $BUILD_PATH/proc
+mount -t sysfs none $BUILD_PATH/sys
 
 #---modify image---
-# modify image in chroot environment
-chroot ${BUILD_PATH} /bin/bash </devicefiles/chroot-script.sh
-
 # modify/add image files directly
+#FIXME: create dedicated Hypriot .deb package
+# install bash prompt as skeleton files (root and default for all new users)
+cp /devicefiles/etc/skel/{.bash_prompt,.bashrc,.profile} $BUILD_PATH/root/
+cp /devicefiles/etc/skel/{.bash_prompt,.bashrc,.profile} $BUILD_PATH/etc/skel/
+
+# modify image in chroot environment
+chroot $BUILD_PATH /bin/bash </devicefiles/chroot-script.sh
 #---modify image---
 
-umount -l "${BUILD_PATH}/sys" || true
-umount -l "${BUILD_PATH}/proc" || true
-umount -l "${BUILD_PATH}/dev/pts" || true
-umount -l "${BUILD_PATH}/dev" || true
+umount -l $BUILD_PATH/sys || true
+umount -l $BUILD_PATH/proc || true
+umount -l $BUILD_PATH/dev/pts || true
+umount -l $BUILD_PATH/dev || true
 
 # package image rootfs
-tar -czf "${IMAGE_ROOTFS_PATH}" -C "${BUILD_PATH}" .
+tar -czf $IMAGE_ROOTFS_PATH -C $BUILD_PATH .
 
 # create the image and add a single ext4 filesystem
 # --- important settings for ODROID SD card
@@ -72,7 +76,7 @@ tar -czf "${IMAGE_ROOTFS_PATH}" -C "${BUILD_PATH}" .
 #set-verbose true
 guestfish <<EOF
 # create new image disk
-sparse /${IMAGE_NAME} ${ROOT_PARTITION_SIZE}
+sparse /$IMAGE_NAME $ROOT_PARTITION_SIZE
 run
 part-init /dev/sda mbr
 part-add /dev/sda primary 2048 -1
@@ -81,17 +85,17 @@ mkfs ext4 /dev/sda1
 
 # import base rootfs
 mount /dev/sda1 /
-tar-in "${IMAGE_ROOTFS_PATH}" / compress:gzip
+tar-in $IMAGE_ROOTFS_PATH / compress:gzip
 EOF
 
 # log image partioning
-fdisk -l /${IMAGE_NAME}
+fdisk -l /$IMAGE_NAME
 
 # ensure that the travis-ci user can access the sd-card image file
 umask 0000
 
 # compress image
-pigz --zip -c "${IMAGE_NAME}" > "${BUILD_RESULT_PATH}/${IMAGE_NAME}.zip"
+pigz --zip -c $IMAGE_NAME > $BUILD_RESULT_PATH/$IMAGE_NAME.zip
 
 # test sd-image that we have built
 rspec --format documentation --color /devicefiles/test
--- devicefiles/chroot-script.sh
+++ devicefiles/chroot-script.sh
@@ -3,25 +3,48 @@ set -x
 
 # device specific settings
 HYPRIOT_DEVICE="ODROID C1"
+HYPRIOT_GROUPNAME="docker"
+HYPRIOT_USERNAME="pirate"
+HYPRIOT_PASSWORD="hypriot"
 
 # set up /etc/resolv.conf
 DEST=$(readlink -m /etc/resolv.conf)
-mkdir -p "$(dirname "${DEST}")"
-echo "nameserver 8.8.8.8" > "${DEST}"
+mkdir -p $(dirname $DEST)
+echo "nameserver 8.8.8.8" > $DEST
 
 # set up odroid repository
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB19BAC9
 echo "deb http://deb.odroid.in/c1/ trusty main" > /etc/apt/sources.list.d/odroid.list
 echo "deb http://deb.odroid.in/ trusty main" >> /etc/apt/sources.list.d/odroid.list
 
-# install parted (for online disk resizing)
 apt-get update
+
+#FIXME: has to be moved to hypriot/os-rootfs
+# install parted (for online disk resizing)
 apt-get install -y parted
+
+#FIXME: has to be moved to hypriot/os-rootfs
+# install sudo (for our default user)
+apt-get install -y sudo
 
 # install odroid kernel
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y u-boot-tools initramfs-tools
 apt-get install -y linux-image-c1
 
+# install Hypriot group and user
+addgroup --system --quiet $HYPRIOT_GROUPNAME
+useradd -m pirate --group $HYPRIOT_GROUPNAME --shell /bin/bash
+echo "$HYPRIOT_USERNAME:$HYPRIOT_PASSWORD" | /usr/sbin/chpasswd
+# add user to sudoers group
+echo "$HYPRIOT_USERNAME ALL=NOPASSWD: ALL" > /etc/sudoers.d/user-$HYPRIOT_USERNAME
+chmod 0440 /etc/sudoers.d/user-$HYPRIOT_USERNAME
+
+#FIXME: has to be removed in hypriot/os-rootfs
+# disable SSH root login
+sed -i 's|PermitRootLogin yes|PermitRootLogin without-password|g' /etc/ssh/sshd_config
+# remove/disable root password
+passwd -d root
+
 # set device label
-echo "HYPRIOT_DEVICE=\"${HYPRIOT_DEVICE}\"" >> /etc/os-release
+echo "HYPRIOT_DEVICE=\"$HYPRIOT_DEVICE\"" >> /etc/os-release
