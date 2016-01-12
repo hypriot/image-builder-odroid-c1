653b623bf74c9cd69e4d0682592f3d33d82763c1
Dieter Reuter <dieter.reuter@me.com>
2016-01-12 21:02:56 +0100

    install Docker tools from Hypriot

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -12,6 +12,10 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB19BAC9
 echo "deb http://deb.odroid.in/c1/ trusty main" > /etc/apt/sources.list.d/odroid.list
 echo "deb http://deb.odroid.in/ trusty main" >> /etc/apt/sources.list.d/odroid.list
 
+# set up hypriot schatzkiste repository
+wget -q https://packagecloud.io/gpg.key -O - | apt-key add -
+echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ wheezy main' > /etc/apt/sources.list.d/hypriot.list
+
 apt-get update
 
 # install odroid kernel
@@ -20,6 +24,19 @@ apt-get install -y u-boot-tools initramfs-tools
 # make the kernel package create a copy of the current kernel here
 touch /boot/uImage
 apt-get install -y linux-image-c1
+
+# setup docker default configuration for ODROID C1
+# --get upstream config
+wget -q -O /etc/default/docker https://github.com/docker/docker/raw/master/contrib/init/sysvinit-debian/docker.default
+# --enable aufs by default
+sed -i '/#DOCKER_OPTS/a \
+DOCKER_OPTS="--storage-driver=aufs -D"' /etc/default/docker
+
+# install hypriot packages for using docker
+apt-get install -y \
+  docker-hypriot \
+  docker-compose \
+  docker-machine
 
 # set device label
 echo "HYPRIOT_DEVICE=\"$HYPRIOT_DEVICE\"" >> /etc/os-release
